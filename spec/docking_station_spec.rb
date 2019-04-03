095d6c08e3528bb3283dfda405323c1a94816d60
sarpong <abasimisarpong@rocketmail.com>
2019-04-03 22:31:03 +0100

    Test suite for release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,5 +3,9 @@ require 'docking_station'
 RSpec.describe DockingStation do
   describe '#release_bike' do
     it {is_expected.to respond_to(:release_bike)}
+    it ' gets a bike' do
+      bike = subject.release_bike
+      expect(bike).to be_working
+    end
   end
 end
