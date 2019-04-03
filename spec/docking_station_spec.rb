ca0e50905a36d1f3518fea02e791d5fb68dce3b8
sarpong <abasimisarpong@rocketmail.com>
2019-04-03 23:13:58 +0100

    Wrote a test for docking bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station'
+require 'bike'
 
 RSpec.describe DockingStation do
   describe '#release_bike' do
@@ -6,6 +7,16 @@ RSpec.describe DockingStation do
     it ' gets a bike' do
       bike = subject.release_bike
       expect(bike).to be_working
+    end
+  end
+
+  describe '#dock_bike' do
+    it {is_expected.to respond_to(:dock_bike)}
+    it {is_expected.to respond_to(:dock_bike).with(1).arguments}
+    it 'must dock bike' do
+      bike = Bike.new
+      expect(subject.dock_bike(bike)).to eq(bike)
+      expect(subject.bike).to eq(bike)
     end
   end
 end
