b8c5c733f34980c75442a763c5390e5ebeba68e0
sarpong <abasimisarpong@rocketmail.com>
2019-04-04 16:21:24 +0100

    updated docking station to raise error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,8 +2,10 @@ require_relative 'bike'
 
 class DockingStation
     attr_reader :bike
+
     def release_bike
-      Bike.new
+      raise'No Bikes Available' unless @bike
+      @bike
     end
     
     def dock_bike(bike)
