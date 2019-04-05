d06039986e2ae199e9005e099b252ad73e290be3
sarpong <abasimisarpong@rocketmail.com>
2019-04-05 14:54:10 +0100

    docking station only allows users to dock max 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,16 +2,22 @@ require_relative 'bike'
 
 class DockingStation
     attr_reader :bikes_available
+    BIKE_MAX_CAPACITY = 20
+
     def initialize
       @bikes_available = []
     end
 
     def release_bike
       raise'No Bikes Available' if @bikes_available.empty?
-      @bikes_available
+      @bikes_available.pop
     end
     
     def dock_bike(bike)
-        @bikes_available << bike
+        if @bikes_available.size >= BIKE_MAX_CAPACITY
+          raise 'Docking station is full'
+        else  
+          @bikes_available << bike
+        end
     end
 end
